a0a53520bc0e97d66fdb8446b7922af04d0cef78
yuichi toyama <yuichi-toyama@ase.co.jp>
2025-06-27 17:05:09 +0900

    stub追加

--- typings/scipy/signal.pyi
+++ typings/scipy/signal.pyi
@@ -1,5 +1,7 @@
 from typing import Tuple
 from typing import Any
+from typing import Optional
+from typing import Union
 import numpy as np
 
 def butter(N: int, Wn: float, btype: str = ..., analog: bool = ...) -> Tuple[np.ndarray[Any, Any], np.ndarray[Any, Any]]: ...
@@ -7,4 +9,10 @@ def filtfilt(
 	b: np.ndarray[Any, Any],
 	a: np.ndarray[Any, Any],
 	x: np.ndarray[Any, Any]
+) -> np.ndarray[Any, Any]: ...
+
+def wiener(
+	im: np.ndarray[Any, Any],
+	mysize: Union[int, tuple[int, ...]] = 3,
+	noise: Optional[float] = None
 ) -> np.ndarray[Any, Any]: ...
